4b83d5887ab7adf95394b0f45ec5b6b658460f24
goncalosard <innsiiiider@hotmail.com>
2022-12-11 19:55:04 +0000

    test

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git add -A
 git commit -m 'New deployment'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:goncalosard/portefolio.git master:main
+git push -f git@github.com:goncalosard/portefolio.git master:gh-pages
 
 cd -
