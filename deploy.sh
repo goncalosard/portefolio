07be685eca0c7778caa0e2a071a2e110694aed90
goncalosard <innsiiiider@hotmail.com>
2022-12-30 15:39:11 +0000

    deployment changes

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,13 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'goncalosard.com' > CNAME
+# echo 'goncalosard.com' > CNAME
 
 git init
 git add -A
 git commit -m 'New deployment'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:goncalosard/portefolio.git master:gh-pages
+git push -f git@github.com:goncalosard/portefolio.git main
 
 cd -
